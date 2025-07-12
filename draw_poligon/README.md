bdae0cd890b84a86b54c5e4ea0410bc72dd22e47
Nesstor07 <erneram1975@gmail.com>
2025-07-11 18:29:28 -0600

    Agrego proyectos como carpetas normales

---
+++ draw_poligon/README.md
@@ -0,0 +1,2 @@
+# draw_poligon_and_fill_it
+![Image OUT.PNG](out.png)
---
+++ game_of_life/src/main.rs
@@ -0,0 +1,11 @@
+mod framebuffer;
+mod line;
+use raylib::prelude::*;
+use framebuffer::framebuffer;
+use line::line;
+
+
+fn main() {
+    
+    println!("Hello, world!");
+}
